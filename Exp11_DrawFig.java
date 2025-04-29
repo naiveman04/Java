34d5f17845f2b11c2745b1edcff3ba042aedea21
Vedant Narawadkar <vedinarawade@gmail.com>
2025-04-29 07:48:36 +0530

    Update Exp11_DrawFig.java

--- Exp11_DrawFig.java
+++ Exp11_DrawFig.java
@@ -9,21 +9,26 @@ public class DrawFigures extends JPanel{
 		JFrame f= new JFrame("Experiment 11");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.getContentPane().add(new DrawFigures());
-		f.setSize(290, 325);
-		f.setLocation(550, 25);
+		f.setSize(500, 500);
 		f.setVisible(true);
+		f.setBackground(Color.white);
 	}
 
 	public void paint(Graphics g) {
 		g.setColor(Color.RED);
-		g.drawLine(150, 50, 90, 50);
+		g.drawLine(150, 50, 200, 100);
 		
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(Color.GREEN);
-		g2.drawRect(60, 100, 180, 60);
+		g2.drawRect(60, 100, 300, 120);
 		
 		Graphics2D g3 = (Graphics2D) g;
 		g3.setColor(Color.BLUE);
 		g3.drawOval(60, 20, 180, 60);
+		
+		Graphics2D g4 = (Graphics2D) g;
+		g4.setColor(Color.yellow);
+		g4.setFont(new Font("SANS_SERIF", Font.PLAIN, 36));
+		g4.drawString("Hello", 400, 60);
 	}
 }
